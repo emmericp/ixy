b5f7d27c0352f1a066c0f9c0db862963eb718cb6
pudelkoM <maximilian.pudelko@tum.de>
2018-01-17 11:53:45 +0100

    Implement virtio driver

--- src/driver/device.c
+++ src/driver/device.c
@@ -1,10 +1,22 @@
 #include "device.h"
 #include "driver/ixgbe.h"
+#include "driver/virtio.h"
+#include "pci.h"
 
 struct ixy_device* ixy_init(const char* pci_addr, uint16_t rx_queues, uint16_t tx_queues) {
-	if (true /* Detection magic */) {
-		return ixgbe_init(pci_addr, rx_queues, tx_queues);
+	// Read PCI configuration space
+	int config = pci_open_resource(pci_addr, "config");
+	uint16_t vendor_id = read_io16(config, 0);
+	uint16_t device_id = read_io16(config, 2);
+	uint32_t class_id = read_io32(config, 8) >> 24;
+	close(config);
+	if (class_id != 2) {
+		error("Device %s is not a NIC", pci_addr);
+	}
+	if (vendor_id == 0x1af4 && device_id >= 0x1000) {
+		return virtio_init(pci_addr, rx_queues, tx_queues);
 	} else {
-		error("Unsupported device");
+		// Our best guess is to try ixgbe
+		return ixgbe_init(pci_addr, rx_queues, tx_queues);
 	}
 }
--- src/pci.h
+++ src/pci.h
@@ -3,6 +3,9 @@
 
 #include <stdint.h>
 
+void remove_driver(const char* pci_addr);
+void enable_dma(const char* pci_addr);
 uint8_t* pci_map_resource(const char* bus_id);
+int pci_open_resource(const char* pci_addr, const char* resource);
 
-#endif //IXY_PCI_H
+#endif // IXY_PCI_H
