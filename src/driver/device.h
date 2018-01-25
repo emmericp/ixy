654bc39de5dbbe98c70a25136c5e4fbdcd802dc2
Paul Emmerich <emmericp@net.in.tum.de>
2018-01-25 02:02:16 +0100

    explain magic container_of macro

--- src/driver/device.h
+++ src/driver/device.h
@@ -14,14 +14,21 @@ struct device_stats;
 
 /**
  * container_of - cast a member of a structure out to the containing structure
+ * Adapted from the Linux kernel.
+ * This allows us to expose the same struct for all drivers to the user's
+ * application and cast it to a driver-specific struct in the driver.
+ * A simple cast would be sufficient if we always store it at the same offset.
+ * This macro looks more complicated than it is, a good explanation can be
+ * found at http://www.kroah.com/log/linux/container_of.html
  * @ptr:	the pointer to the member.
  * @type:	the type of the container struct this is embedded in.
  * @member:	the name of the member within the struct.
  *
  */
-#define container_of(ptr, type, member) ({			\
-	const typeof(((type *)0)->member) * __mptr = (ptr);	\
-	(type *)((char *)__mptr - offsetof(type, member)); })
+#define container_of(ptr, type, member) ({\
+	const typeof(((type*)0)->member)* __mptr = (ptr);\
+	(type*)((char*)__mptr - offsetof(type, member));\
+})
 
 struct ixy_device {
 	const char* pci_addr;
