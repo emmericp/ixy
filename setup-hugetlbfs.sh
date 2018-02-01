2cf9fcbdb5fe440965260ed3623c3555586299ec
Paul Emmerich <emmericp@net.in.tum.de>
2018-02-01 03:01:39 +0100

    properly mount hugetlbfs
    turns out it's important to mount it like this: you can't
    delete "stuck" huge pages to free them after a crash otherwise.

--- setup-hugetlbfs.sh
+++ setup-hugetlbfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p /mnt/huge
-(mount | grep /mnt/huge) > /dev/null || mount -t hugetlbfs nodev /mnt/huge
+(mount | grep /mnt/huge) > /dev/null || mount -t hugetlbfs hugetlbfs /mnt/huge
 for i in {0..7}
 do
 	if [[ -e "/sys/devices/system/node/node$i" ]]
