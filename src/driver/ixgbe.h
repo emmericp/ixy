3dae6fa22d3116227aa76ed6c1e19c3429fefda1
Simon Ellmann <simon.ellmann@tum.de>
2020-10-14 01:16:42 +0200

    Implement L2 address getter and setter for ixgbe

--- src/driver/ixgbe.h
+++ src/driver/ixgbe.h
@@ -16,6 +16,8 @@ struct ixgbe_device {
 
 struct ixy_device* ixgbe_init(const char* pci_addr, uint16_t rx_queues, uint16_t tx_queues, int interrupt_timeout);
 uint32_t ixgbe_get_link_speed(const struct ixy_device* dev);
+struct mac_address ixgbe_get_mac_addr(const struct ixy_device* dev);
+void ixgbe_set_mac_addr(struct ixy_device* dev, struct mac_address mac);
 void ixgbe_set_promisc(struct ixy_device* dev, bool enabled);
 void ixgbe_read_stats(struct ixy_device* dev, struct device_stats* stats);
 uint32_t ixgbe_tx_batch(struct ixy_device* dev, uint16_t queue_id, struct pkt_buf* bufs[], uint32_t num_bufs);
